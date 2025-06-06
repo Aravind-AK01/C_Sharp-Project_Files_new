a70dd9103d706512c253b7fb434fba401d8efbfc
Aravind-AK01 <aravindhbala88@gmail.com>
2025-06-06 12:23:13 +0530

    Second commit

--- Program.cs
+++ Program.cs
@@ -1,14 +1,4 @@
 ﻿using System;
-int[,] a = { { 1, 2, 3 }, { 4, 5, 6 } };
-Console.WriteLine(a[0, 2]);
-
-
-
-
-
-
-
-
 
 /*
 string[] cars = new string[4];
@@ -16,7 +6,7 @@ string[] cars = new string[4] { "AK", "BK" };
 string[] cars = new string[4] { "Volvo", "BMW" };
 string[] cars = { "Volvo", "BMW" };*/
 
-/*using System;
+/*
 int[] a = { 3, 3, 4, 4, 3 };
 int total_credits = a[0] + a[1] + a[2] + a[3] + a[4];
 double gpa = 0;
@@ -30,7 +20,7 @@ gpa /= total_credits;
 Console.WriteLine($"Final GPA: {gpa}");
 */
 
-/*Console.WriteLine("Enter the Name of the Student: ");
+Console.WriteLine("Enter the Name of the Student: ");
 string student_name = Console.ReadLine();
 Console.WriteLine("English 101: ");
 double Eng_grade = Convert.ToDouble(Console.ReadLine());
@@ -44,4 +34,4 @@ Console.WriteLine("Pyschology 101: ");
 double Pyschology_grade = Convert.ToDouble(Console.ReadLine());
 double total_credits = eng_credit + Algebra_credit + Biology_credit + Computer_credit + Pyschology_credit;
 double Gpa = ((eng_credit * Eng_grade) + (Algebra_credit * Algebra_grade) + (Biology_credit * Biology_grade) + (Computer_credit * Computer_grade) + (Pyschology_credit * Pyschology_grade)) / total_credits;
-Console.WriteLine($"Final GPA: {Gpa}");*/
+Console.WriteLine($"Final GPA: {Gpa}");
